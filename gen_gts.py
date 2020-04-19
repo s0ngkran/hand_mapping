937e522baca5fcf6a7a50d2a108a2daf524ae42b
s0ngkran <52753974+s0ngkran@users.noreply.github.com>
2020-04-19 18:59:11 +0700

    Create gen_gts.py

---
+++ gen_gts.py
@@ -0,0 +1,21 @@
+def gen_gts(cx,cy,width,height,sigma):
+    # cx -> center x
+    # cy -> center y
+    # width, height of image
+    # sigma -> size of heat
+    emt = torch.zeros([width,height])
+    x, y = torch.where(emt==0)
+    distx = (x-cx).float()
+    disty = (y-cy).float()
+    dist = distx**2+disty**2
+    ans = torch.exp(-(dist/sigma**2).float())
+    ans = ans.reshape([width,height])
+    
+    # example code
+    # import torch
+    # import matplotlib.pyplot as plt
+    # a = gen_gts(200,200,400,600,10)
+    # plt.imshow(a)
+    # plt.show()
+    
+    return ans
